a332fc7400858c4e768edacd0729f7778ac6766f
YaPutya <nikita12-0101@mail.ru>
2023-04-07 14:39:02 +0300

    working!

--- server/routes/dalle.routes.js
+++ server/routes/dalle.routes.js
@@ -1,6 +1,6 @@
 import express from 'express';
 import * as dotenv from 'dotenv';
-import { Configuration, OpenAIApi} from 'openai';
+import { Configuration, OpenAIApi } from 'openai';
 
 dotenv.config();
 
@@ -13,8 +13,8 @@ const config = new Configuration({
 const openai = new OpenAIApi(config);
 
 router.route('/').get((req, res) => {
-  res.status(200).json({ message: "Hello from DALL.E ROUTES" })
-})
+  res.status(200).json({ message: 'Hello from DALL.E ROUTES' });
+});
 
 router.route('/').post(async (req, res) => {
   try {
@@ -24,7 +24,7 @@ router.route('/').post(async (req, res) => {
       prompt,
       n: 1,
       size: '1024x1024',
-      response_format: 'b64_json'
+      response_format: 'b64_json',
     });
 
     const image = response.data.data[0].b64_json;
@@ -32,8 +32,8 @@ router.route('/').post(async (req, res) => {
     res.status(200).json({ photo: image });
   } catch (error) {
     console.error(error);
-    res.status(500).json({ message: "Something went wrong" })
+    res.status(500).json({ message: 'Something went wrong' });
   }
-})
+});
 
 export default router;
